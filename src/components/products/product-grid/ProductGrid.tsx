239e21bad7d4a117fd011d72dea9e298c5c9796f
Juan Giusti <jmgiusti91@gmail.com>
2025-01-11 20:04:30 -0300

    feat: layout menu

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/interfaces";
 import React from "react";
+import { ProductGridItem } from "./ProductGridItem";
 
 interface Props {
   products: Product[];
@@ -9,7 +10,7 @@ export const ProductGrid = ({ products }: Props) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
       {products.map((product) => (
-        <span key={product.slug}>{product.title}</span>
+        <ProductGridItem key={product.slug} product={product} />
       ))}
     </div>
   );
